3e8d78cfaccad19d74f013a0a7e49d65c85f7954
Juliherms Vasconcelos <j.a.vasconcelos321@gmail.com>
2023-05-29 22:45:25 -0300

    feat: testes com buffers

--- streams/buffer.js
+++ streams/buffer.js
@@ -1,1 +1,5 @@
 //eh a representação de um espaco na memoria de um computador
+//como executar: node streams/buffer.js
+const buf = Buffer.from("ok")
+
+console.log(buf.toJSON()) //printa o valor em hexadecimal
